ada45b1560ece181b1c8106c0d7f99424c3acac8
daniel <danie@LAPTOP-DCQ1J05M>
2023-10-27 15:55:13 -0300

    Se crean clases reclamo, area y motivo

---
+++ dominio/Reclamo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Reclamo
+    {
+        public int IdReclamo { get; set; }
+
+        public Motivo IdMotivo { get; set; }
+        public string Descripion { get; set; }
+
+        public Area Area { get; set; }
+
+    }
+}
