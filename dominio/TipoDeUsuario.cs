6d3d33caa4e667c527a49397629166dab0fde259
daniel <danie@LAPTOP-DCQ1J05M>
2023-10-31 08:50:34 -0300

    Se agregan clases usuario y tipo de usuario

---
+++ dominio/TipoDeUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class TipoDeUsuario
+    {
+        public int IdTipoDeUsuario { get; set; }
+        public string TipoUsuario { get; set; }
+
+    }
+}
---
+++ dominio/usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string loginUsuario { get; set; }
+        public string email { get; set; }
+        public TipoDeUsuario TipoDeUsuario { get; set; }      
+    }
+}
