823bf070895f26fe1ecbe18b0bf9930256f11f3f
daniel <danie@LAPTOP-DCQ1J05M>
2023-11-03 18:34:11 -0300

    Se modifica el contenido para mostrar estadisticas generales

--- Main.aspx.cs
+++ Main.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using dominio;
+using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,12 @@ namespace TP_Final_equipo_27
 {
     public partial class _Default : Page
     {
+        List<Incidente> ListaIncidentes = new List<Incidente>();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            IncidenteNegocio incidenteNegocio = new IncidenteNegocio();
+            ListaIncidentes = incidenteNegocio.listarIncidentes();
+            lblCantidad.Text = ListaIncidentes.Count.ToString();
         }
     }
 }
